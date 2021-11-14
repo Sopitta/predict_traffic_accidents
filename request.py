f90300081cff66139f066d59144b38e70afe8504
Sopitta <tsopitta@gmail.com>
2021-11-14 22:39:56 +0200

    removed unwanted files

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-#url = 'http://localhost:5000/api'
-url = 'http://localhost:5000'
-r = requests.post(url,json={'Category':'Alkoholunfälle','Type':'insgesamt','Year':'2021','Month':'01'})
-print(r.json())
